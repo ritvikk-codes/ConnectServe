503230c963694c976e18d5d4a0c1ca1508d7357d
ritvikk-codes <ritvikk1507@gmail.com>
2026-08-24 15:25:13 +0530

    feat: add server/middleware/validationMiddleware.js

---
+++ server/middleware/validationMiddleware.js
@@ -0,0 +1,21 @@
+const { validationResult } = require('express-validator');
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const errorDetails = errors.array().map(err => ({
+      field: err.path || err.param,
+      message: err.msg,
+      value: err.value,
+    }));
+
+    return res.status(400).json({
+      success: false,
+      message: errorDetails[0]?.message || 'Validation error',
+      errors: errorDetails,
+    });
+  }
+  next();
+};
+
+module.exports = { validate };
