dec1d63b7e6063789a8af2e0d4e6d20188e9a05e
ritvikk-codes <ritvikk1507@gmail.com>
2026-08-24 15:24:34 +0530

    feat: add client/src/services/authService.js

---
+++ client/src/services/authService.js
@@ -0,0 +1,23 @@
+import api from './api';
+
+export const authService = {
+  login: async (credentials) => {
+    const response = await api.post('/auth/login', credentials);
+    return response.data;
+  },
+
+  register: async (userData) => {
+    const response = await api.post('/auth/register', userData);
+    return response.data;
+  },
+
+  getMe: async () => {
+    const response = await api.get('/auth/me');
+    return response.data;
+  },
+
+  updatePassword: async (passwordData) => {
+    const response = await api.put('/auth/password', passwordData);
+    return response.data;
+  },
+};
